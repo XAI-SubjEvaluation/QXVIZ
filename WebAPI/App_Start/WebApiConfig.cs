684d8232c7541ec6bc6edbd3be241e72b6c29d71
Yuqing Yang <75032255+XAI-SubjEvaluation@users.noreply.github.com>
2023-07-07 15:09:31 +0200

    Add files via upload

---
+++ WebAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,42 @@
+﻿using ExplainableAIWebApi.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ExplainableAIWebApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            // New code for enabling cors
+            var cors = new EnableCorsAttribute("*", "*", "*");
+           /* {
+                SupportsCredentials = true
+            };*/
+            config.EnableCors();
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            
+            config.Routes.MapHttpRoute(
+              name: "ControllerAndActionOnly",
+              routeTemplate: "api/{controller}/{action}",
+              defaults: new { },
+              constraints: new { action = @"^[a-zA-Z]+([\s][a-zA-Z]+)*$" });
+
+            config.Routes.MapHttpRoute(
+              name: "DefaultActionApi",
+              routeTemplate: "api/{controller}/{action}/{id}",
+              defaults: new { id = RouteParameter.Optional }
+            );
+
+
+            //add this for basic authentication filter for the web api 
+            config.Filters.Add(new BasicAuthenticationAttribute());
+        }
+    }
+}
